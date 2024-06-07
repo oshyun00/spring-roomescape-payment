e6b4f12400cf89b9d9effad0207f596efa27535f
hyun <osh123chm@gmail.com>
2024-06-07 17:26:38 +0900

    feat: 초기 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,49 @@
+-- 회원
+INSERT INTO member (name, email, password, role)
+VALUES
+    ('카키', 'kaki@email.com', '1234', 'USER'),
+    ('어드민', 'admin@email.com', '1234', 'ADMIN'),
+    ('솔라', 'solar@email.com', '1234', 'USER'),
+    ('브라운', 'brown@email.com', '1234', 'USER'),
+    ('네오', 'neo@email.com', '1234', 'USER'),
+    ('브리', 'bre@email.com', '1234', 'USER'),
+    ('포비', 'pobi@email.com', '1234', 'USER'),
+    ('구구', 'googoo@email.com', '1234', 'USER'),
+    ('토미', 'tomi@email.com', '1234', 'USER'),
+    ('리사', 'risa@email.com', '1234', 'USER');
+ALTER TABLE member ALTER COLUMN id RESTART WITH 11;
+
+-- 에약 시간
+INSERT INTO reservation_time (start_at)
+VALUES
+    ('10:00'),
+    ('10:30'),
+    ('11:00'),
+    ('11:30'),
+    ('12:00'),
+    ('12:30'),
+    ('13:00'),
+    ('13:30'),
+    ('14:00'),
+    ('14:30');
+ALTER TABLE reservation_time ALTER COLUMN id RESTART WITH 11;
+
+-- 테마
+INSERT INTO theme (name, description, thumbnail)
+VALUES
+    ('공포', '무서워요', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('SF', '미래', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('원숭이 사원', '원숭이들의 공격', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('나가야 산다', '빨리 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('좀비 사태', '좀비들의 공격', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('공포의 놀이공원', '놀이공원 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('지하실', '지하실 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('타이타닉', '타이타닉에서 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('미술관을 털어라', '미술관을 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ( '바이러스', '바이러스를 막으세요', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ( '프리즌 브레이크', '감옥을 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ( '아즈텍 신전', '신전을 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ( '우주 정거장', '우주 정거장을 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ( '치과의사', '치과의사를 피해 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ( '비밀요원', '비밀요원이 돼 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+ALTER TABLE theme ALTER COLUMN id RESTART WITH 16;
